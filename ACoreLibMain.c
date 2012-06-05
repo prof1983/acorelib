f3f6d45e79d489efb0539ba17cea5d7ed9e52ec5
Prof1983 <prof1983@yandex.ru>
2012-06-05 09:26:12 +0600

    Updated

--- ACoreLibMain.c
+++ ACoreLibMain.c
@@ -1,14 +1,20 @@
 /* ACoreLib
 Author(Prof1983 prof1983@ya.ru)
 Created(28.05.2012)
-LastMod(28.05.2012)
+LastMod(05.06.2012)
 Version(0.0.0)
 */
 
 #include <windows.h>
 
+#define A_BUILD_DLL
+
+#include "APlugins.h"
+#include "AStrings.h"
+
 #pragma argsused
 int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved)
 {
+    MessageBoxExA(0, NULL, NULL, 0, 0);
 	return 1;
 }
